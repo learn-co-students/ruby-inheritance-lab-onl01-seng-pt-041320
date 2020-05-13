a1ee778e2e324a570bc547a8172d439770de3f19
marcusLau <lau.marcus76@gmail.com>
2020-05-13 14:48:50 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+
+    attr_accessor :knowledge
+
+    # what happens to the student first/last name..? 
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(s)
+        @knowledge << s
+    end
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,10 @@
+class User
+
+    attr_accessor :first_name, :last_name
+
+    def initialize(first="Tracy", last="Morgan")
+        @first_name = first
+        @last_name = last
+    end
+    
+end
