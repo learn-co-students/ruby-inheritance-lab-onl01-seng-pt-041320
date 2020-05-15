8dcda5ee5a4f73d3db1d325669fb99662b1a545b
Travis Gandy <travis_gandy@yahoo.com>
2020-05-15 17:21:48 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
-
-end
+class Student < User 
+  def initialize
+      @knowledge = []
+  end 
+  def learn(string_information)
+      @knowledge << string_information
+  end 
+  def knowledge
+      @knowledge
+  end 
+end 
---
+++ lib/user.rb
@@ -0,0 +1,5 @@
+class User 
+  attr_writer :first_name, :last_name
+  attr_reader :first_name, :last_name
+  
+end 
