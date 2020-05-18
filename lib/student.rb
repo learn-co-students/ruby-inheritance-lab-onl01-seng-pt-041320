05ac99c4bf1cb35bf61b90fa36dd81eb3a1982cd
eulis01 <eulis01@gmail.com>
2020-05-18 12:16:39 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
-
+require 'pry'
+require_relative "./user.rb"
+class Student < User
+# inherits from the User class and has a first and last name.
+    def initialize 
+        @knowledge = []
+    end
+# takes in an argument of a string of knowledge and adds it to the student's knowledge array.
+    def learn(element)
+        @knowledge << element
+    end
+# returns that student's knowledge array.
+    def knowledge
+        @knowledge
+    end
 end
